246372ed875b659c9bcaba7edf320382195e37bd
Woohyuk-Lee04 <woohlee@ttu.edu>
2025-12-17 19:28:43 -0600

    Learn JS functions

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,6 +1,17 @@
+function sayHello() {
+    console.log("Hello");
+}
+
+var sayBye = function() {
+    console.log("Bye");
+}
+
 4 + 3;
 
 if(4 + 3 === 7) {
     alert("You are Smart!");
     console.log("Hellooooo");
 }
+
+sayHello();
+sayBye();
